6488331af751542696af4a37d7f9c35729915bb9
zidanhafiz <nadizsonata@gmail.com>
2024-02-21 18:38:38 +0800

    Add firebase

---
+++ utils/firebase/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
---
+++ utils/firebase/getData.ts
@@ -0,0 +1,52 @@
+import {
+  DocumentData,
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAt,
+} from 'firebase/firestore';
+import { db } from './firebase';
+
+export const getAllDocuments = async () => {
+  const data: DocumentData[] = [];
+  const querySnapshot = await getDocs(collection(db, 'projects'));
+
+  querySnapshot.forEach((doc) => {
+    data.push(doc.data());
+  });
+
+  const totalItems = data.length;
+  const itemsPerPage = 4;
+  const totalPage = Math.ceil(totalItems / itemsPerPage);
+
+  return { data, totalItems, itemsPerPage, totalPage };
+};
+
+export const getAllDocumentsByPage = async (page: number) => {
+  const data: DocumentData[] = [];
+
+  // Get all documents
+  const queryAll = query(collection(db, 'projects'), orderBy('createdAt'));
+  const documentSnapshots = await getDocs(queryAll);
+
+  // Get the first of visible document
+  const firstIndex = page * 4 - 4;
+  const firstDoc = documentSnapshots.docs[firstIndex];
+
+  // Querying the documents which starts with the first of visible document
+  const queryPage = query(
+    collection(db, 'projects'),
+    orderBy('createdAt'),
+    limit(4),
+    startAt(firstDoc)
+  );
+  const querySnapshot = await getDocs(queryPage);
+
+  querySnapshot.forEach((doc) => {
+    data.push(doc.data());
+  });
+
+  return data;
+};
