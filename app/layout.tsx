6ccf842cdcf33557fa972798261e16a5942de8d7
zidanhafiz <nadizsonata@gmail.com>
2024-02-23 14:27:34 +0800

    Add manually metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,24 @@ import ModalProvider from '@/context/showModal';
 export const metadata: Metadata = {
   title: 'Zidan Hafiz Rapiyani | Portfolio',
   description: `Explore my personal portfolio website to discover my projects, skills, and experience as a web developer. Learn about my work in frontend and backend development using Next.js, React.js, Express.js, PostgreSQL, Firebase, and Supabase. Let's connect and discuss how I can help bring your ideas to life!`,
+  metadataBase: new URL('https://zidanhafiz.vercel.app'),
+  openGraph: {
+    images: [
+      {
+        url: './opengraph-image.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: './opengraph-image.png',
+        width: 1800,
+        height: 1600,
+        alt: 'Issue Tracker',
+      },
+    ],
+    locale: 'id_ID',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
