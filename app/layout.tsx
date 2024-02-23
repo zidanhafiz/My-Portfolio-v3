df7894a9e3a37a2fd677d0dbb1dc80bb389f831c
zidanhafiz <nadizsonata@gmail.com>
2024-02-23 14:02:07 +0800

    Add metadata and opengraph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import ModalProvider from '@/context/showModal';
 
 export const metadata: Metadata = {
   title: 'Zidan Hafiz Rapiyani | Portfolio',
-  description: `This is Zidan's personal portfolio website.`,
+  description: `Explore my personal portfolio website to discover my projects, skills, and experience as a web developer. Learn about my work in frontend and backend development using Next.js, React.js, Express.js, PostgreSQL, Firebase, and Supabase. Let's connect and discuss how I can help bring your ideas to life!`,
 };
 
 export default function RootLayout({
